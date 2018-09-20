1579c759e0d8fa141eab4b5bad91e7dcad199747
Jose Lopez <jalopez1423@gmail.com>
2018-09-20 10:19:59 -0700

    reviewing let and cosnt declerations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
-class User {
-    register() {
-        console.log('User Registered...');
-    }
-}
+"use strict"
+
+const colors = [];
+colors.push('red');
+colors.push('blue');
+console.log(colors);
+
+colors = 'Green'; // Will throw error
+
+// function testVar() {
+//     var a = 30;
+//     if (true) {
+//         var a = 50;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// using let
+// function testLet() {
+//     let a = 30;
+//     if(true) {
+//         let a = 50;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+
+// testLet();
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// Old way of doing it
+// var a = 'Test 1';
+// console.log(a);
+//
+// let b = 'Test 2';
+// console.log(b);
+// global scope var & let are essentially the same
+
+//create a couple functions
+
+
+// class User {
+// //     register() {
+// //         console.log('User Registered...');
+// //     }
+// // }
